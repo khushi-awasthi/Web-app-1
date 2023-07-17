265b66d118a981205ecea3b0bf77dda849be7f79
Khushi <khushiawasthi8834@gmail.com>
2023-07-17 02:17:05 -0600

    app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st                       
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter,ImageDraw,ImageFont
 import os                                                                
 
 #create a folder images
@@ -19,10 +19,39 @@ upload=st.file_uploader(
 )
 if upload is not None:
     save_image(upload)
-    st.image(
-        upload,
-        caption='Uploaded Image',
-        use_column_width=True
-    ) 
+    img=Image.open(upload)
+    col1,col2=st.columns(2)
+    filters=['contour','emboss','edge_enhance','blur','smooth','sharpen']
+    option=st.sidebar.selectbox('select a filter',filters)
+    col1.image(upload,caption='Uploaded image',use_column_width=True)
+    if option=='contour':
+        col2.image(img.filter(ImageFilter.CONTOUR),caption='Contour Filter',use_column_width=True)
+    if option=='emboss':
+        col2.image(img.filter(ImageFilter.EMBOSS),caption='Emboss Filter',use_column_width=True)
+    if option=='edge_enhance':
+        col2.image(img.filter(ImageFilter.EDGE_ENHANCE),caption='Edge_enhance Filter',use_column_width=True)
+    if option=='blur':
+        col2.image(img.filter(ImageFilter.BLUR),caption='Blur Filter',use_column_width=True)       
+    if option=='smooth':
+        col2.image(img.filter(ImageFilter),caption='Smooth Filter',use_column_width=True)  
+    if option=='sharpen':
+        col2.image(img.filter(ImageFilter.SHARPEN),caption='Sharpen  Filter',use_column_width=True) 
+    if option=='find edges':
+        col2.image(img.filter(ImageFilter .FIND_EDGES), caption='Find Edge',use_column_width=True)
 
-    filter=['contour','emboss','edge_enhance','blur']                                                                                                
+    message=st.sidebar.text_input("Enter a message",value="MONDAY")
+    font_size=st.sidebar.number_input("Enter font size",value=20)
+    font_color=st.sidebar.color_picker("Select font color")
+    c1,c2=st.sidebar.columns(2)
+    x=c1.number_input('X coordinate',value=img.width//2)
+    y=c2.number_input('Y coordinate',value=img.height//2)
+
+    #copy img to another
+    imgc=img.copy()
+    draw = ImageDraw.Draw(imgc)
+    font=ImageFont.truetype('My font.ttf',font_size)
+    draw.text((x,y),message,font_color,font)
+    st.image(imgc,use_column_width=True)
+
+
+                                                                                                                
